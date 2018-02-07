e3729e6c5097c9966c623940115ca68699c0fc96
Erin Walker <erinb0317@hotmail.com>
2018-02-07 02:52:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
     song_name = filename.split(" - ")[1]
 
     new_song = self.new(song_name)
-    new_song.ARTIST = Artist.find_or_create_by_name(artist_name)
+    new_song.artist = Artist.find_or_create_by_name(artist_name)
     new_song.artist.add_song(new_song)
     new_song
   end
