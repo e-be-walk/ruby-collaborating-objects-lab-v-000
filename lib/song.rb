41459ca36edd125628e76ab09db1d9841cd83b17
Erin Walker <erinb0317@hotmail.com>
2018-02-07 02:38:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,11 @@
 class Song
   attr_accessor :artist, :name
 
-  def artist=(name)
+  def initialize(name)
+    @name = name
+  end
+
+  def artist=(artist)
     if (self.artist.nil?)
       self.artist = Artist.new(name)
     else
