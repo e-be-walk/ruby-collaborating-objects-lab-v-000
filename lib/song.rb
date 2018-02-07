9007d03a5d65533e8f84c94c3a2dcafe9eb0b361
Erin Walker <erinb0317@hotmail.com>
2018-02-07 02:51:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,17 +5,14 @@ class Song
     @name = name
   end
 
-  def artist=(artist)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
-  end
-
   def self.new_by_filename(filename)
-    song = self.new
-    song.title = filename.split(" - ")[1]
-    song
+    puts filename
+    artist_name = filename.split(" - ")[0]
+    song_name = filename.split(" - ")[1]
+
+    new_song = self.new(song_name)
+    new_song.ARTIST = Artist.find_or_create_by_name(artist_name)
+    new_song.artist.add_song(new_song)
+    new_song
   end
 end
